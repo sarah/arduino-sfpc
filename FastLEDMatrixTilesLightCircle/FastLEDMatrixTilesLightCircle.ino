73a10db3fd4f09aa084a9073eece1a54fa4ba4ca
sarah <sarahg.gray@gmail.com>
2018-04-15 18:50:24 -0400

    starting with states and logging

--- FastLEDMatrixTilesLightCircle/FastLEDMatrixTilesLightCircle.ino
+++ FastLEDMatrixTilesLightCircle/FastLEDMatrixTilesLightCircle.ino
@@ -3,24 +3,30 @@
 
 // Change the next defines to match your matrix type and size
 #define DATA_PIN            6
-
 #define COLOR_ORDER         GRB
 #define CHIPSET             WS2811
-
 #define MATRIX_TILE_WIDTH   8 // width of EACH NEOPIXEL MATRIX (not total display)
 #define MATRIX_TILE_HEIGHT  8 // height of each matrix
-
 #define MATRIX_TILE_H       2  // number of matrices arranged horizontally
 #define MATRIX_TILE_V       2  // number of matrices arranged vertically
-
 #define MATRIX_WIDTH        (MATRIX_TILE_WIDTH*MATRIX_TILE_H)
 #define MATRIX_HEIGHT       (MATRIX_TILE_HEIGHT*MATRIX_TILE_V)
-
-#define MATRIX_TYPE        (MTX_MATRIX_TOP | MTX_MATRIX_LEFT | MTX_MATRIX_ROWS | MTX_MATRIX_ZIGZAG | MTX_TILE_TOP | MTX_TILE_LEFT | MTX_TILE_ROWS); 
-
+#define MATRIX_TYPE         (MTX_MATRIX_TOP | MTX_MATRIX_LEFT | MTX_MATRIX_ROWS | MTX_MATRIX_ZIGZAG | MTX_TILE_TOP | MTX_TILE_LEFT | MTX_TILE_ROWS); 
 #define MATRIX_SIZE         (MATRIX_WIDTH*MATRIX_HEIGHT)
 #define MATRIX_PANEL        (MATRIX_WIDTH*MATRIX_HEIGHT)
 #define NUM_LEDS            (MATRIX_WIDTH*MATRIX_HEIGHT)
+
+#define WAITING 0
+#define SOFT_PURR 1
+#define MORE_PURR 2
+#define NO_STOP 3
+#define RETRACT 4
+
+bool stateWaiting =  true;
+bool stateSoftPurr = false;
+bool stateMorePurr = false;
+bool stateNoStop = false;
+bool stateRetract = false;
 
 // create our matrix based on matrix definition
 cLEDMatrix<MATRIX_TILE_WIDTH, MATRIX_TILE_HEIGHT, HORIZONTAL_MATRIX, MATRIX_TILE_H, MATRIX_TILE_V> leds;
@@ -31,42 +37,74 @@ int inByte = 0;
 void setup()
 {
   Serial.begin(57600);
-  
-  
+//  Serial.println("Hello");
   FastLED.addLeds<CHIPSET, DATA_PIN,  COLOR_ORDER>(leds[0],leds.Size()).setCorrection(TypicalSMD5050);
-  FastLED.setBrightness(50);
+  FastLED.setBrightness(5);
   FastLED.clear(true);
- 
-
-  
-
 //  // Japanese Flag
 //  leds.DrawFilledRectangle(0, 0, leds.Width() - 1, leds.Height() - 1, CRGB(255, 255, 255));
 //  uint16_t r = min((leds.Width() - 1) / 2, (leds.Height() - 1) / 2) - 1;
 //  leds.DrawFilledCircle((leds.Width() - 1) / 2, (leds.Height() - 1) / 2, r, CRGB(255, 0, 0));
 //  FastLED.show();
 //  delay(5000);
-
-
 }
+#define LOGGING false
+// FORCE SENSOR (PETTING)
+int pressurePin = A0;
+int force;
 
+void Log(String data){
+  if(LOGGING){
+    Serial.println(data);
+  }
+}
 
 void loop()
 {
-  PurrLights();
-//  FastLED.show();
+  if(stateWaiting){
+    Log("Waiting");
+    showWaitingState();
+  }
   
+  // if get an input from a pin, trigger softPurr. TODO figure out sensor later
+  force = analogRead(pressurePin);
+  Log((String)force);
+  
+  if(force > 50 && force < 100){
+    Log("MOVING TO SOFT PURR");
+    stateWaiting = false;
+    stateSoftPurr = true;
+  }
+  
+  if(stateSoftPurr){
+    Log("Softpurr");
+    Serial.write(SOFT_PURR);
+    //SoftPurrLights(); 
+    ShowTestLight(0,0);
+  } 
 }
 
-void PurrLights(){
+void showWaitingState(){
+  // TODO get the circle where I want it.
+    leds.DrawFilledCircle((leds.Width() - 1) / 4, (leds.Height() - 1) / 4, 1, CRGB(255, 0, 0));
+    FastLED.show();
+}
+void SoftPurrLights(){
   while(Serial.available() > 0){
     inByte = Serial.read();
     float radius = (float(inByte)/255.0)*14.0;
+      ShowTestLight(0,0);
 //    LightAreaCircle(radius);
     FastLED.show();
   }
 }
 
+void ShowTestLight(int x,int y){
+  Log("SHOW TEST LIGHT");
+  FastLED.clear();
+  leds(x,y) = CRGB(255,0,0);
+  FastLED.show();
+}
 void LightAreaCircle2(float radius){
   FastLED.clear();
   // TODO how to do the brightness here
